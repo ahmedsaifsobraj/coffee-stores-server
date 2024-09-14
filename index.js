648dce0d9312f409bc81d7fc82ecab266a14ed68
ahmedsaifsobraj <ahmedsaifsobraj@gmail.com>
2024-09-14 15:39:37 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,130 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+app.use(express.json())
+app.use(cors())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yc8ov.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const coffeeCollection = client.db('coffeeDB').collection('coffee');  //for coffees api
+    const userCollection = client.db('coffeeDB').collection('user');    //for users api
+
+    //for coffees api
+    app.get('/coffee', async (req, res) => {
+      const cursor = coffeeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+    app.get('/coffee/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeCollection.findOne(query);
+      res.send(result);
+    })
+    app.post('/coffee', async (req, res) => {
+      const newCoffee = req.body;
+      console.log(newCoffee);
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    })
+
+   
+
+    app.put('/coffee/:id', async(req, res) => {
+      const id = req.params.id;
+      const updatedCoffee = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+
+      const coffee = {
+        $set: {
+          name: updatedCoffee.name,
+          quantity: updatedCoffee.quantity,
+          supplier: updatedCoffee.supplier,
+          taste: updatedCoffee.taste,
+          category: updatedCoffee.category,
+          details: updatedCoffee.details,
+          photo: updatedCoffee.photo
+        }
+      };
+      const result = await coffeeCollection.updateOne(filter,coffee,options);
+      res.send(result);
+    })
+
+    app.delete('/coffee/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeCollection.deleteOne(query);
+      res.send(result);
+    })
+
+     //for users api
+     app.post('/user',async(req,res)=>{
+      const user = req.body;
+      console.log(user);
+      const result = userCollection.insertOne(user);
+      res.send(result);
+
+    })
+
+    app.get('/user',async(req,res)=>{
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.delete('/user/:id', async(req,res)=>{
+      const id = req.params.id;
+      const find ={_id: new ObjectId(id)};
+      const result = await userCollection.deleteOne(find);
+      res.send(result);
+    })
+
+    app.patch('/user',async(req,res)=>{
+      const user = req.body;
+      const filter = { email: user.email };
+      const updatedDoc ={
+        $set:{
+          lastLoggedAt:user.lastLoggedAt
+        }
+      };
+      const result = await userCollection.updateOne(filter,updatedDoc);
+      res.send(result);
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('coffee store is running')
+})
+app.listen(port, () => {
+  console.log(`coffee server is running on:${port}`)
+})
